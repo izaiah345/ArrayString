5ce718451da67422d42122d868012add857bc6f6
izaiah365 <izaiah.rodriguez@patelhs.org>
2020-11-02 18:47:48 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    string [] food = {"Pizza", "Taco", "Hamburger","Hotdog","Fries","Chicken","Cake"}; 
+
+    for(int i = 0; i < 6; i++){
+      System.out.println(food[i]);
+    }
+
+
+    food [6]= "cheese";
+
+   System.out.print("\n here is our updated list: \n")
+
+    for(int i = 0; i <food.length; i++){
+      System.out.println(food[i]);
+    }
+  }
+}
